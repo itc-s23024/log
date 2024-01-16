85341c3b07ff5ff6804323e3fc87298c209ee649
又吉陸宇 <s23024@std.it-college.ac.jp>
2024-01-16 06:54:19 +0000

    9.5 p268~271 記事一覧を作成することができた1/16(火)

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,14 +1,27 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Posts from 'components/posts'
 
-const Blog = () => {
+const Blog = ({ posts }) => {
   return (
     <Container>
       <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
       <Hero title='Blog' subtitle='Recent Posts' />
+
+      <Posts posts={posts} />
     </Container>
   )
+}
+
+export async function getStaticProps () {
+  const posts = await getAllPosts()
+  return {
+    props: {
+      posts: posts
+    }
+  }
 }
 
 export default Blog
