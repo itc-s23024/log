b5788b618976f5a6f764fb0dcd668568e3c193d8
又吉陸宇 <s23024@std.it-college.ac.jp>
2024-01-16 07:44:55 +0000

    9.6 p272~275 記事一覧にnext/imageでアイキャッチ画像を追加できた1/16(火)

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,10 @@ import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
 
 const Blog = ({ posts }) => {
   return (
@@ -17,6 +21,14 @@ const Blog = ({ posts }) => {
 
 export async function getStaticProps () {
   const posts = await getAllPosts()
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
   return {
     props: {
       posts: posts
